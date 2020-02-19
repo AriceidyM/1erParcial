ab9af733a5817c9a67b3ac8aad83cb8f980a8a2a
Ariceidy Maria Ure�a Rosario <ariceidy_urena@ucne.edu.do>
2020-02-19 18:23:10 -0400

    Avanzando examen

---
+++ Models/Inscripcion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial.Models
+{
+    public class Inscripcion
+    {
+        public int InscripcionId { get; set; }
+        public string Semestre { get; set; }
+        public string Limite { get; set; }
+        public string Tomados { get; set; }
+        public int Dispoibles { get; set; }
+
+        public Inscripcion()
+        {
+            InscripcionId = 0;
+            Semestre = string.Empty;
+            Limite = string.Empty;
+            Tomados = string.Empty;
+            Dispoibles = 0;
+        }
+
+    }
+}
